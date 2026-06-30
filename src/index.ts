72dfd9b1741422459a404f6e8f261347f4eb9011
mochamadgalih <mochamadglh@gmail.com>
2026-06-30 18:42:09 +0700

    (+): add cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-
+import cors from "cors";
 import express from "express";
 import router from "./routes/index";
 import { errorHandler } from "./errors/errorHandler";
@@ -11,6 +11,7 @@ const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use("/api/v1", router);
 app.use(requestlogger);
