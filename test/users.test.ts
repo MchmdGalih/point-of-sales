8a8242e29c3a8c44954cbbb64b080e12b30cdb19
mochamadgalih <mochamadglh@gmail.com>
2026-07-12 23:23:48 +0700

    add: test with jest and ts-jest

--- test/users.test.ts
+++ test/users.test.ts
@@ -1,11 +1,11 @@
-import { describe, expect, test } from "@jest/globals";
+import { describe, expect, it, test } from "@jest/globals";
 import supertest from "supertest";
 import { app } from "../src/index";
 import { logger } from "../src/config/logger";
 
-describe("POST /api/v1/user", () => {
-  test("shoud reject register new user if request is invalid", async () => {
-    const response = await supertest(app).post("/api/v1/user").send({
+describe("POST/api/v1/user/create", () => {
+  it("should reject register now user if request in valid", async () => {
+    const response = await supertest(app).post("/api/v1/user/create").send({
       username: "",
       email: "",
       password: "",
@@ -13,6 +13,19 @@ describe("POST /api/v1/user", () => {
 
     logger.debug(response.body);
     expect(response.status).toBe(400);
-    expect(response.body.error).toBeDefined();
+    expect(response.body.errors).toBeDefined();
+  });
+
+  it("should register new user if request is valid", async () => {
+    const response = await supertest(app).post("/api/v1/user/create").send({
+      username: "user test 1",
+      email: "usertest01@mailcom",
+      password: "usertest123",
+    });
+
+    logger.debug(response.body);
+    expect(response.status).toBe(201);
+    expect(response.body.data.username).toBe("user test 1");
+    expect(response.body.data.email).toBe("usertest01@mailcom");
   });
 });
